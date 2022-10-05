a83dbd029afdefad51c267d7811f513ea4bda4bf
Nivas <jawaharsrinivs77@gmail.com>
2022-10-05 10:01:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+var codingSkills = {c:"75%",java:"80%",python:"90%",dart:"60%",javaScript:"80%"};
+var FrontEndApplicationSkills = {html:"95%",css:"95%",bootstrap:"80%",reactJs:"80%"};
+var BackendApplicationSkills = {Firebase:"70%",php:"20%",Nodejs:"85%"};
+
